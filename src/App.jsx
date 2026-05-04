cd85d0f245a50375c28898d783ddb2e60d216dfa
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-05-04 20:37:28 +0200

    esercizio di base fatto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 
-function App() {
-  const articleList = [
+const articleListPrimary = [
     {
       id: 1,
       title: 'Naruto Uzumaki',
@@ -20,15 +19,48 @@ function App() {
       content: 'Sasuke Uchiha (うちはサスケ, Uchiha Sasuke) è un ninja del Villaggio della Foglia e unico sopravvissuto del Clan Uchiha dopo il massacro perpetrato dal fratello Itachi Uchiha. Dopo la promozione a Genin, viene assegnato al Team Kakashi assieme a Sakura Haruno e Naruto Uzumaki, suo rivale e migliore amico.',
       img: 'https://static.wikia.nocookie.net/naruto/images/f/f3/Sasuke_Uchiha_profilo.png/revision/latest/scale-to-width-down/250?cb=20201010201022&path-prefix=it'
     }
-  ]
+  ];
 
+function App() {
+  const emptyArticle = {
+    title: '',
+    content: '',
+    img: ''
+  }
 
+  const [article, setArticle] = useState(emptyArticle);
+  const [finalArticleList, setFinalArticleList] = useState(articleListPrimary);
+
+  function changeHandler(event){
+    const newArticle = {
+      ...article,
+      [event.target.name]: event.target.value
+    }
+    setArticle(newArticle);
+  }
+
+  function submitHandler(event){
+    event.preventDefault();
+
+    const articleToAdd = {
+      ...article,
+      id: crypto.randomUUID()
+    }
+
+    const newArticleList = [...finalArticleList, articleToAdd];
+    setFinalArticleList(newArticleList);
+    
+    console.log(finalArticleList);
+  }
+
+  
+  
 
   return <>
     <h1>Ciao</h1>
     <div className="container">
       <div className="card-wrapper row justify-content-between">
-        {articleList.map(current => {
+        {finalArticleList.map(current => {
           return <div className="card my-card col-4" key={current.id}>
             <img src={current.img} />
             <div className="card-body pb-5">
@@ -40,8 +72,23 @@ function App() {
         })}
       </div>
     </div>
-
-    <button className='btn btn-primary'>click</button>
+    <div className="container">
+      <form className='my-form' onSubmit={submitHandler}>
+        <div className="mb-3">
+          <label className='form-lable'>URL imamgine</label>
+          <input onChange={changeHandler} type="text" className='form-control' name='img'/>
+        </div>
+        <div className="mb-3">
+          <label className='form-lable'>Nome Personaggio</label>
+          <input onChange={changeHandler} type="text" className='form-control' name='title'/>
+        </div>
+        <div className="mb-3">
+          <p><label className='form-lable' style={{marginBottom: 0}}>Contenuto dell'articolo</label></p>
+          <textarea onChange={changeHandler} name="content" id='description' rows="4" cols="50"></textarea>
+        </div>
+        <button type='submit' className='btn btn-primary'>Aggiungi</button>
+      </form>
+    </div>
   </>
     ;
 }
